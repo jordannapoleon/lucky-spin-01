5407bf2117a30b169d3f9c6f69905c6dd49c64bd
Jordan Napoleon <jord@Jordans-MacBook-Pro-307.local>
2026-01-08 15:22:21 -0800

    completes part 6, spinner logic and variable lucky number

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -18,16 +18,15 @@ namespace LuckySpin.Controllers
 
 
             System.Text.StringBuilder htmlToShow =  
-                new System.Text.StringBuilder("<body><h1>Lucky Spin - by 'Rylee & Jordan'</h1><button onclick='history.go(0)'>Spin</button>");
+                new System.Text.StringBuilder("<body><h1>Lucky " + luck +  " - by 'Rylee & Jordan' </h1><button onclick='history.go(0)'>Spin</button>");
             htmlToShow.Append("<div>" + spin[0] + "</div>");
             htmlToShow.Append("<div>" + spin[1] + "</div>");
             htmlToShow.Append("<div>" + spin[2] + "</div>");
 
-            if (spin.Contains(7))
+            if (spin.Contains(luck))
             {
                 htmlToShow.Append("<img src='http://studentfolders.cascadia.edu/itweb285/LuckySpinCoins.jpg'/></body>");   
             }
-
 
             //TODO: Modify this to use the string builder's response string as the Content property's value
             return new ContentResult { Content = htmlToShow.ToString(), ContentType="text/html"};
